dc4ade7c7d7898d64715b454980ea441234b821c
JessCodes <jessicafellison@gmail.com>
2016-12-19 21:21:27 -0800

    add index placeholder and controller actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,26 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
