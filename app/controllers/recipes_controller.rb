b40abcf902ce80f5fd2e87194190cdbf37b40b89
JessCodes <jessicafellison@gmail.com>
2016-12-20 06:51:39 -0800

    add show recipes in desc order

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
+    @recipe = Recipe.all.order("created_at DESC")
   end
 
   def show
