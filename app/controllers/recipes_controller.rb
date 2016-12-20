b2c6e7f813d15eae51ea216d7da69d6aa6817ebb
JessCodes <jessicafellison@gmail.com>
2016-12-20 07:07:58 -0800

    add edit and delete recipe functions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,22 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted recipe"
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:title, :description)
